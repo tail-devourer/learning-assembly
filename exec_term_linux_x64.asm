b1608c097390c9bd55bb0d836422c1dd1a91eaa0
Sanjangeet Singh <sanjangeetsingh97@gmail.com>
2026-01-14 22:12:00 -0600

    Fixed the issue

--- exec_term_linux_x64.asm
+++ exec_term_linux_x64.asm
@@ -2,8 +2,8 @@ section .text
     global _start
 
     _start:
-        push 0x00736c2f
-        push 0x6e69622f
+        mov rax, 0x00736c2f6e69622f
+        push rax
 
         mov rax, 59     ; sys_execve
         mov rdi, rsp
