921e6067f4eb9261efb43e06e50184738cc7c7aa
Sanjangeet Singh <sanjangeetsingh97@gmail.com>
2026-01-14 22:03:05 -0600

    Ok this is not working

--- exec_term_linux_x64.asm
+++ exec_term_linux_x64.asm
@@ -2,8 +2,7 @@ section .text
     global _start
 
     _start:
-        push 0x00000068
-        push 0x7361622f
+        push 0x00736c2f
         push 0x6e69622f
 
         mov rax, 59     ; sys_execve
